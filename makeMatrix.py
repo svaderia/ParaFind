67448d0ae7ac79203ed8a00e691c913f9292569e
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2018-03-31 01:38:17 +0530

    Added extra information in test case

--- makeMatrix.py
+++ makeMatrix.py
@@ -8,5 +8,11 @@ a.sort(axis=0)
 a.sort(axis=1)
 a = np.ceil((10**order)*a)
 a = a.astype(int)
-np.savetxt('test.csv', a ,fmt = '%{}.{}d'.format(order, order), delimiter=',')
-
+# np.savetxt('test.csv', a ,fmt = '%{}.{}d'.format(order, order), delimiter=',')
+with open("test.csv", 'w') as f:
+    f.write("{} {}\n".format(dim, order))
+    for row in a:
+        for ele in row[:-1]:            
+            f.write('{:0{align}d}, '.format(ele, align=order))
+        f.write('{:0{align}d}'.format(row[-1], align=order))
+        f.write("\n")
