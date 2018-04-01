2bfcf242441b797f20454597e6c1cf5e34954357
jbnerd <abhivjoshi.aj@gmail.com>
2018-04-01 16:50:41 +0530

    Changed the binary search further for extra case handling and optimization on copy memory for columns

--- binary_seach.c
+++ binary_seach.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 int special_bs(int* arr, int key, int low, int high){
+	if(arr[high] > key){
+		return -1;
+	}
+
 	while(low < high){
 		int mid = low + (high - low)/2;
 		if(arr[mid] < key){
@@ -16,18 +22,33 @@ int special_bs(int* arr, int key, int low, int high){
 }
 
 int search_in_column(int** matrix, int key, int col_num, int low_row, int num_rows){
-	int* column = (int*) malloc(num_rows * sizeof(int));
-	for(int i = 0; i < num_rows; i++){
-		column[i] = matrix[i + low_row][col_num];
+	if(matrix[high] > key){
+		return -1;
 	}
 
-	int intermediate_result = special_bs(column, key, 0, num_rows - 1);
+	int low = low_row, high = low_row + num_rows;
+	int intermediate_result;
+	while(low < high){
+		int mid = low + (high - low)/2;
+		if(matrix[mid][col_num] < key){
+			low = mid + 1;
+		}else if(matrix[mid][col_num] > key){
+			high = mid;
+		}else{
+			intermediate_result = mid;
+		}
+	}
+	intermediate_result = low;
 	return intermediate_result + low_row;
 }
 
 int search_in_row(int** matrix, int key, int row_num, int low_col, int num_cols){
 	int intermediate_result = special_bs(matrix[row_num], key, low_col, low_col + num_cols);
-	return low_col + intermediate_result;
+	if(intermediate_result != -1){
+		return low_col + intermediate_result;
+	}else{
+		return -1;
+	}	
 }
 
 int main(){
@@ -36,7 +57,7 @@ int main(){
 	for(int i = 0; i < 10; i++){
 		arr[i] = 2 * i;
 	}
-	int result = special_bs(arr, 9, 0, 19);
+	int result = special_bs(arr, 18294, 0, 19);
 	printf("%d\n", arr[result]);
 
 	return 0;
