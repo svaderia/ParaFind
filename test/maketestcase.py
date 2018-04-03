07033b27d70a7957b14f6ba2bbb283277c917ec7
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2018-04-03 18:10:15 +0530

    Answer file generation code added.

--- test/maketestcase.py
+++ test/maketestcase.py
@@ -8,10 +8,21 @@ a.sort(axis=0)
 a.sort(axis=1)
 a = np.ceil((10 * dim**2) * a)
 a = a.astype(int)
+total_queries = 10**4
+
 with open("./test/test.csv", 'w') as f:
     f.write("{}\n".format(dim))
     f.write("\n".join([" ".join([str(ele) for ele in row]) for row in a]))
 
 
-with open("test/quary.in", "w") as f:
-    f.write("\n".join([str(random.randint(1, 10 * dim**2)) for _ in range(10**4) ]))
+with open("test/query.in", "w") as f:
+    f.write("{}\n".format(total_queries))    
+    arr = [str(random.randint(1, 10 * dim**2)) for _ in range(total_queries) ]
+    f.write("\n".join(arr))
+
+with open("test/answer.out", "w") as f:
+    for ele in arr:
+        if int(ele) in a:
+            f.write("1\n")
+        else:
+            f.write("0\n")
