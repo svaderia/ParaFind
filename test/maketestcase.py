c343338edb44e49ff0acc7a5c5d04d7e79e43519
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2018-04-03 14:12:36 +0530

    Update in maketesecase.py
    [1] order variable removed.
    [2] changed path of test.csv. Reason: to directly run the file from parent makefile.

--- test/maketestcase.py
+++ test/maketestcase.py
@@ -7,10 +7,6 @@ a.sort(axis=0)
 a.sort(axis=1)
 a = np.ceil((10 * dim**2) * a)
 a = a.astype(int)
-with open("test.csv", 'w') as f:
-    f.write("{}\n".format(dim, order))
-    for row in a:
-        for ele in row[:-1]:
-            f.write('{} '.format(ele, align=order))
-        f.write('{}'.format(row[-1], align=order))
-        f.write("\n")
+with open("./test/test.csv", 'w') as f:
+    f.write("{}\n".format(dim))
+    f.write("\n".join([" ".join([str(ele) for ele in row]) for row in a]))
