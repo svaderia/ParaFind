6d2e713a1edd2f3756910587d52d2108f8fbf4c1
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2018-04-03 14:47:36 +0530

    Added query generation code

--- test/maketestcase.py
+++ test/maketestcase.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import random
 
 dim = int(sys.argv[1])
 a = np.random.rand(dim, dim)
@@ -10,3 +11,7 @@ a = a.astype(int)
 with open("./test/test.csv", 'w') as f:
     f.write("{}\n".format(dim))
     f.write("\n".join([" ".join([str(ele) for ele in row]) for row in a]))
+
+
+with open("test/quary.in", "w") as f:
+    f.write("\n".join([str(random.randint(1, 10 * dim**2)) for _ in range(10**4) ]))
