0337db38c702f2208912b5191179382213bbf92a
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2018-04-05 13:18:53 +0530

    Test case generation updated.

--- test/maketestcase.py
+++ test/maketestcase.py
@@ -8,21 +8,36 @@ a.sort(axis=0)
 a.sort(axis=1)
 a = np.ceil((10 * dim**2) * a)
 a = a.astype(int)
+
 total_queries = 10**4
 
-with open("./test/test.csv", 'w') as f:
+with open("./test/test.csv", "w") as f:
     f.write("{}\n".format(dim))
-    f.write("\n".join([" ".join([str(ele) for ele in row]) for row in a]))
+    for row in a:
+        for ele in row:
+            f.write("{}\n".format(ele))
+        # f.write("{}\n".format(row[-1]))
+    # f.write("\n".join([" ".join([str(ele) for ele in row]) for row in a]))
 
+arr = [str(random.randint(1, 10 * dim**2)) for _ in range(total_queries) ]
 
 with open("test/query.in", "w") as f:
     f.write("{}\n".format(total_queries))    
-    arr = [str(random.randint(1, 10 * dim**2)) for _ in range(total_queries) ]
     f.write("\n".join(arr))
 
-with open("test/answer.out", "w") as f:
-    for ele in arr:
-        if int(ele) in a:
-            f.write("1\n")
-        else:
-            f.write("0\n")
+a = a.flatten()
+# print(a.shape)
+# a = set([str(ele) for ele in a])
+# b = set()
+# for ele in a:
+#     b.add(ele)
+# # free(a)
+
+# print(type(a))
+# with open("test/answer.out", "w") as f:
+#     for ele in arr:
+#         # print(ele, end=", ")
+#         if int(ele) in b:
+#             f.write("1\n")
+#         else:
+#             f.write("0\n")
