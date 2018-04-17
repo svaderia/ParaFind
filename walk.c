64c7e77578d030d986fb03da1d4ffc0d1e07eb70
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2018-04-18 00:56:59 +0530

    Final Driver code
    [1] Parallelization of the algorithm (PS: Was very confusing and still scope of improvement. :confused:)
    [2] Code to generate the matrix added.

--- walk.c
+++ walk.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <stdbool.h>
 #include "binary.h"
 #include "myRead.h"
 
+bool found = false;
 
 struct position {
     int row;
@@ -11,6 +13,34 @@ struct position {
 };
 typedef struct position position;
 
+int** make_matrix(int size){
+    int** matrix = (int**) malloc(size * sizeof(int*));
+    int i;
+    for( i = 0; i < size; i ++){
+        matrix[i] = (int*) malloc(size * sizeof(int));
+    }
+
+    int j = 0, k = 0;
+    int num = 1;
+
+    for(k = 0; k < size; k ++){
+        i = 0;
+        for(j = k; j >= 0; j --){
+            matrix[i][j] = num++;
+            i ++;
+        }
+    }
+
+    for(k = 1; k < size; k ++){
+        j = size - 1;
+        for(i = k; i < size; i++){
+            matrix[i][j] = num++;
+            j --;
+        }
+    }
+
+    return matrix;
+}
 
 position* splitWalk(int** mat, int query, position* pt1, position* pt2, int depth){
     
@@ -19,6 +49,7 @@ position* splitWalk(int** mat, int query, position* pt1, position* pt2, int dept
     
     if(query == mat[pt1->row][pt1->coll])
     {
+        found = true;
         loc1 = (position*) malloc(sizeof(position));
         loc1-> row = pt1->row;
         loc1->coll = pt1->coll;
@@ -28,6 +59,7 @@ position* splitWalk(int** mat, int query, position* pt1, position* pt2, int dept
     }
     else if(query == mat[pt2->row][pt2->coll])
     {
+        found = true;
         loc1 = (position*) malloc(sizeof(position));
         loc1-> row = pt2->row;
         loc1->coll = pt2->coll;
@@ -37,62 +69,74 @@ position* splitWalk(int** mat, int query, position* pt1, position* pt2, int dept
 
     int num_rows = pt1->row < pt2->row ? (pt2->row - pt1->row):(pt1->row - pt2->row);
     
-    if(num_rows > 1)
+    if(num_rows > 1000)
     {
-        position* mid = (position*) malloc(sizeof(position));
-        mid->row = (pt1->row + pt2->row)/2;
-        int low_coll = pt1->coll < pt2->coll ? pt1->coll : pt2->coll;
-        int num_coll = pt1->coll < pt2->coll ? (pt2->coll - pt1->coll + 1):(pt1->coll - pt2->coll + 1);
-        mid->coll = search_in_row(mat, query,mid->row ,low_coll , num_coll );
+        if(!found){
+            position* mid = (position*) malloc(sizeof(position));
+            mid->row = (pt1->row + pt2->row)/2;
+            int low_coll = pt1->coll < pt2->coll ? pt1->coll : pt2->coll;
+            int num_coll = pt1->coll < pt2->coll ? (pt2->coll - pt1->coll + 1):(pt1->coll - pt2->coll + 1);
+            mid->coll = search_in_row(mat, query,mid->row ,low_coll , num_coll );
 
-        // printf("Point 1 row: %d, coll:%d val:%d \n",pt1->row,pt1->coll,mat[pt1->row][pt1->coll]);
-        // printf("Point 2 row: %d, coll:%d  val:%d \n",pt2->row,pt2->coll,mat[pt2->row][pt2->coll]);
-        // printf("sent to search:row:%d start coll:%d, num_of_coll:%d ",mid->row ,low_coll , num_coll);
-        // printf("depth: %d \n", depth);
-        // printf("mid elem %d  row: %d, coll:%d\n\n",mat[mid->row][mid->coll],mid->row,mid->coll);
+            // printf("Point 1 row: %d, coll:%d val:%d \n",pt1->row,pt1->coll,mat[pt1->row][pt1->coll]);
+            // printf("Point 2 row: %d, coll:%d  val:%d \n",pt2->row,pt2->coll,mat[pt2->row][pt2->coll]);
+            // printf("sent to search:row:%d start coll:%d, num_of_coll:%d ",mid->row ,low_coll , num_coll);
+            // printf("depth: %d \n", depth);
+            // printf("mid elem %d  row: %d, coll:%d\n\n",mat[mid->row][mid->coll],mid->row,mid->coll);
 
-        // #pragma omp single 
-        // {
-            #pragma omp task shared(loc1)
+            // #pragma omp single 
+            // {
+            #pragma omp task shared(found, loc1)
                 loc1 = splitWalk(mat, query,pt1,mid,depth+1);         //left recursion
 
-            #pragma omp task shared(loc2)
+            #pragma omp task shared(found, loc2)
                 loc2 = splitWalk(mat, query,mid,pt2,depth+1);       //right recursion
+
+            #pragma omp taskwait
+
+            free(mid);
+            if(loc1 == NULL)
+                return loc2;
+            else
+                return loc1;
+        }else{
+            return NULL;
+        }
         // }
-        
-        #pragma omp taskwait
-        
-        free(mid);
-        if(loc1 == NULL)
-            return loc2;
-        else
-            return loc1;
-        
     }
     else
     {
-        int i = pt1->row, j = pt1-> coll;
+        if(!found){
+            int i = pt1->row, j = pt1-> coll;
 
-        //printf("Point 1 row: %d, coll:%d val:%d \n",pt1->row,pt1->coll,mat[pt1->row][pt1->coll]);
-        //printf("Point 2 row: %d, coll:%d  val:%d \n",pt2->row,pt2->coll,mat[pt2->row][pt2->coll]);
-        //printf("depth: %d \n\n", depth);
+            //printf("Point 1 row: %d, coll:%d val:%d \n",pt1->row,pt1->coll,mat[pt1->row][pt1->coll]);
+            //printf("Point 2 row: %d, coll:%d  val:%d \n",pt2->row,pt2->coll,mat[pt2->row][pt2->coll]);
+            //printf("depth: %d \n\n", depth);
 
-        while(i > pt2->row && j < pt2-> coll){
-            //printf("Element %d  at row: %d, coll:%d, traversed during walk\n",mat[i][j],i,j);
-            if(query == mat[i-1][j]){
-                // printf("Element %d found at row: %d, coll:%d\n",mat[i-1][j],i-1,j);
-                // printf("1\n");
-                loc1 = (position*) malloc(sizeof(position));
-                loc1->coll = j;
-                loc1->row = i; 
-                break;
+            while(i > pt2->row && j < pt2-> coll){
+                //printf("Element %d  at row: %d, coll:%d, traversed during walk\n",mat[i][j],i,j);
+                if(query == mat[i-1][j]){
+                    // printf("Element %d found at row: %d, coll:%d\n",mat[i-1][j],i-1,j);
+                    // printf("1\n");
+                    loc1 = (position*) malloc(sizeof(position));
+                    loc1->coll = j;
+                    loc1->row = i; 
+                    break;
+                }
+                else if(query > mat[i-1][j])
+                    j++;
+                else
+                    i--;
             }
-            else if(query > mat[i-1][j])
-                j++;
-            else
-                i--;
+
+            if(loc1 != NULL){
+                found = true;
+            }
+            
+            return loc1;
+        }else{
+            return NULL;
         }
-        return loc1;
     }    
 }
 
@@ -105,22 +149,23 @@ int main(int argc, char* argv[]){
     omp_set_num_threads(num_threads);
     omp_set_nested(1);
 
-    if(argc > 2) filename = argv[2];
-    else{
-        filename = (char*) malloc(24 * sizeof(char));
-        memset(filename, '\0', 24);
-        strcpy(filename , "test/test.csv");
-    }
+    // if(argc > 2) filename = argv[2];
+    // else{
+    //     filename = (char*) malloc(24 * sizeof(char));
+    //     memset(filename, '\0', 24);
+    //     strcpy(filename , "test/test.csv");
+    // }
     
     int** mat;
-    int n;
+    int n = atoi(argv[2]);
     double wtime;
     
-    mat = get_matrix(mat, filename, &n);
+    // mat = get_matrix(mat, filename, &n);
+    mat = make_matrix(n);
 
     // Reading Queries
     int* queries, total_queries;
-    char* query_path = "test/query.in";
+    char* query_path = "query.in";
     queries = read_queries(queries, query_path, &total_queries);
 
     position* point1 = (position*) malloc(sizeof(position));
@@ -128,10 +173,13 @@ int main(int argc, char* argv[]){
     position* val = (position*) malloc(sizeof(position));
 
     double starttime = omp_get_wtime();
-
-    for(int i = 0; i < total_queries; i++){   
+    
+    int i;
+    
+    for( i = 0; i < total_queries; i++){   
         wtime = omp_get_wtime();
         int query = queries[i] ;
+        found = false;
 
         if(query >= mat[0][0] && query <= mat[n-1][n-1]){
             if(query >= mat[n-1][0]){
@@ -150,9 +198,9 @@ int main(int argc, char* argv[]){
                                 point1->coll = search_in_row(mat, query, n-1, 0, n);
                             //pt2 btm row, pt1 right coll 
                         }
-                        #pragma omp single nowait
+                        #pragma omp single
                         {
-                            #pragma omp task shared(val)
+                            #pragma omp task shared(found, val)
                                 val = splitWalk(mat, query,point1,point2,1);
                         }
                     }
@@ -171,9 +219,9 @@ int main(int argc, char* argv[]){
                                 point2->coll = search_in_row(mat, query, 0, 0,n);
                         }
 
-                        #pragma omp single nowait
+                        #pragma omp single
                         {
-                            #pragma omp task shared(val)
+                            #pragma omp task shared(found, val)
                                 val = splitWalk(mat, query,point1,point2,1);
                         }
                     }
@@ -194,9 +242,9 @@ int main(int argc, char* argv[]){
                             #pragma omp section    
                                 point1->row = search_in_column(mat, query, 0 ,0,n); // search in column
                         }
-                        #pragma omp single nowait
+                        #pragma omp single
                         {
-                            #pragma omp task shared(val)
+                            #pragma omp task shared(found, val)
                                 val = splitWalk(mat, query,point1,point2,1);
                         }
                     }
@@ -213,9 +261,9 @@ int main(int argc, char* argv[]){
                             #pragma omp section
                                 point2->coll = search_in_row(mat, query, 0, 0,n);
                         }
-                        #pragma omp single nowait
+                        #pragma omp single
                         {
-                            #pragma omp task shared(val)
+                            #pragma omp task shared(found, val)
                                 val = splitWalk(mat, query,point1,point2,1);
                         }
                     }
@@ -223,16 +271,16 @@ int main(int argc, char* argv[]){
             }
         }
         else{
-            printf("0\n" );
+            // printf("0\n" );
             continue;
         }
         
         // #pragma omp taskwait
-        if(val == NULL){
-            printf("0\n" );
-        }else{
-            printf("1\n");
-        }
+        // if(val == NULL){
+        //     printf("0\n" );
+        // }else{
+        //     printf("1\n");
+        // }
     }
     double timeTotal = (omp_get_wtime() - starttime);
     printf("time taken %14.7f \t Avg time %14.5f \n",timeTotal , timeTotal/total_queries);
