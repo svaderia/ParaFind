206cde71fc2f9fa8d040d15d03dc27a316e5a6d2
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2018-04-18 00:47:01 +0530

    Commented the code of reading the matrix from the file
    [1] Matrix is generated on the fly in walk.c, So we don't need to read the matrix.
    [2] for loop construct changed

--- myRead.c
+++ myRead.c
@@ -37,43 +37,43 @@ char* getWord(FILE* fp, char* b, int k, int* offset, bool* eof){
     }
 }
 
-int** get_matrix(int** mat, char* path, int* dim){
-    FILE* fp = fopen(path, "r");
-    if(fp == NULL){
-        printf("Fucked");
-    }
+// int** get_matrix(int** mat, char* path, int* dim){
+//     FILE* fp = fopen(path, "r");
+//     if(fp == NULL){
+//         printf("Fucked");
+//     }
 
-    fscanf(fp, "%d", dim);
-    mat = (int**) malloc((*dim) * sizeof(int*));
+//     fscanf(fp, "%d", dim);
+//     mat = (int**) malloc((*dim) * sizeof(int*));
 
-    char* b = (char*) malloc(4096 * sizeof(char));
-    char* word_buffer;
-    int k = 4096;
-    int offset = 0;
-    bool eof = false;
-    memset(b, 0, k);
+//     char* b = (char*) malloc(4096 * sizeof(char));
+//     char* word_buffer;
+//     int k = 4096;
+//     int offset = 0;
+//     bool eof = false;
+//     memset(b, 0, k);
     
-    for(int i = 0; i < *dim; i++){
-        mat[i] = (int*) malloc((*dim) * sizeof(int));
-        for(int j = 0; j < *dim; j++){
-            word_buffer = getWord(fp, b, k, &offset, &eof);
-            mat[i][j] = atoi(word_buffer);
-            free(word_buffer);
-        }
-    }
-    fclose(fp);
-    free(b);
-    return mat;
-}
+//     for(int i = 0; i < *dim; i++){
+//         mat[i] = (int*) malloc((*dim) * sizeof(int));
+//         for(int j = 0; j < *dim; j++){
+//             word_buffer = getWord(fp, b, k, &offset, &eof);
+//             mat[i][j] = atoi(word_buffer);
+//             free(word_buffer);
+//         }
+//     }
+//     fclose(fp);
+//     free(b);
+//     return mat;
+// }
 
-void print_matrix(int** mat, int dim){
-    for(int i = 0; i < dim; i++){
-        for(int j = 0; j < dim; j++){
-            printf("%d\t", mat[i][j] );
-        }
-        printf("\n");
-    }
-}
+// void print_matrix(int** mat, int dim){
+//     for(int i = 0; i < dim; i++){
+//         for(int j = 0; j < dim; j++){
+//             printf("%d\t", mat[i][j] );
+//         }
+//         printf("\n");
+//     }
+// }
 
 int* read_queries(int* queries, char* path, int* total_queries){
     FILE* fp = fopen(path, "r");
@@ -90,8 +90,8 @@ int* read_queries(int* queries, char* path, int* total_queries){
     int offset = 0;
     bool eof = false;
     memset(b, 0, k);
-    
-    for(int i = 0; i < *total_queries; i++){
+    int i;
+    for( i = 0; i < *total_queries; i++){
         word_buffer = getWord(fp, b, k, &offset, &eof);
         queries[i] = atoi(word_buffer);
         free(word_buffer);
